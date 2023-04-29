50dcb5ee71053e14f77049748f26f807d0aa215b
Tugs-Erd <129593011+Tugs-Erd@users.noreply.github.com>
2023-04-29 21:00:10 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,246 @@
+
+
+ //IOI
+
+//  for(var i=1; i<=1; i++){
+//     console.log("IOI")
+//  }
+
+ // n toonii niilber
+ 
+//  var n= +prompt("1 ees hed hurtleh toonii niilberiig oloh ve?")
+//  var niilber = 0;
+//  for(var i=1; i<=n; i++){
+    
+//     niilber = niilber + i;
+     
+//  }
+//  console.log(niilber)
+ 
+ // IOI 3 udaa
+
+//  for(var i=1; i<=3; i++){
+//     console.log(i + ".IOI")
+//  }
+
+ // IOI n udaa hevle
+
+//  var n = +prompt("Ta IOI iig heden udaa hevleh ve?")
+//  for(var i=1; i<=n; i++){
+//     console.log(i + ".IOI")
+//  }
+
+ // n! 
+
+// var n = +prompt("Ta yamar toonii factorialiig oloh ve?")
+// var f;
+// f = 1;
+//  for(var i=1; i<=n; i++){
+//     f = f * i;
+//  }
+//  console.log(f)
+
+ // 100 g n udaa nem
+
+//   var n = +prompt("Ta 100 g hedeer urjih ve?")
+//  for(var i=1; i<=1; i++){
+//     console.log(100 * n)
+//  }
+
+ // a toog n udaa nem
+
+//  var n= +prompt("Ta 50 iig heden udaa nemeh ve?")
+//  var nemeh = 50;
+//  nemsen = nemeh * n;
+//  for(var i=1; i<=1; i++){
+//     console.log(nemsen)
+// }
+
+ // 2 iin  n zereg
+
+// var n =+prompt('Ta 2 iin heden zeregiig harah ve?')
+// var a= 2;
+// hariu = 0;
+//  for(var i=1; i<=n; i++){
+//     hariu = a ** n;
+// }
+//  console.log(hariu);
+
+
+ // a toonii n zereg
+ 
+// var a, n, urj;
+// a = 3;
+// n = 4;
+// urj = 0;
+// for(var i=1; i<=n; i++){
+//     urj = a ** n;
+// }
+// console.log(urj)
+
+ // tentssen / unasan
+
+//  var a;
+//  a = 4;
+//     switch(a){
+//         case 2:
+//         console.log('Unasan')
+//         break;
+//         case 3:
+//         console.log('Unasan')
+//         break;
+//         case 4:
+//         console.log('Tentssen')
+//         break;
+//         case 5:
+//         console.log('Tentssen')
+//         break;
+//     }
+
+ // 5 aas onts ruu
+
+//   var a;
+//  a = 4;
+//     switch(a){
+//         case 2:
+//         console.log('muu')
+//         break;
+//         case 3:
+//         console.log('dund')
+//         break;
+//         case 4:
+//         console.log('sain')
+//         break;
+//         case 5:
+//         console.log('onts')
+//         break;
+//     }
+
+ // Тоон үнэлгээнээс үсгэнд
+
+//  var n = 84;
+//     switch(n){
+//         case n <= 100 && n >= 89:
+//         console.log('A')    
+//         break;
+//         case n <= 89 && n >= 79:
+//         console.log('B')    
+//         break;
+//         case n <= 79 && n >= 69:
+//         console.log('C')    
+//         break;
+//         case n <= 69 && n >= 59:
+//         console.log('D')    
+//         break;
+//         default:
+//             console.log('F');
+// }
+
+ //   Гараг
+
+//  var n = 5;
+
+//      switch(n){
+//         case 1:
+//         console.log('Monday')
+//         break;
+//         case 2:
+//         console.log('Tuesday')
+//         break;
+//         case 3:
+//         console.log('Wednesday')
+//         break;
+//         case 4:
+//         console.log('Tursday')
+//         break;
+//         case 5:
+//         console.log('Friday')
+//         break;
+//         case 6:
+//         console.log('Saturday')
+//         break;
+//         case 7:
+//         console.log('Sunday')
+//         break;
+//         default:
+//             console.log(' 1-7 hoorondiin too bh yostoi.')
+//     }
+
+ // Улирал
+ 
+//  var u = 3;
+//          switch(u){
+//         case 1:
+//         console.log('Winter')
+//         break;
+//         case 2:
+//         console.log('Spring')
+//         break;
+//         case 3:
+//         console.log('Spring')
+//         break;
+//         case 4:
+//         console.log('Spring')
+//         break;
+//         case 5:
+//         console.log('Summer')
+//         break;
+//         case 6:
+//         console.log('Summer')
+//         break;
+//         case 7:
+//         console.log('Summer')
+//         break;
+//         case 8:
+//         console.log('Autumn')
+//         break;
+//         case 9:
+//         console.log('Autumn')
+//         break;
+//         case 10:
+//         console.log('Autumn')
+//         break;
+//         case 11:
+//         console.log('winter')
+//         break;
+//         case 12:
+//         console.log('Winter')
+//         break;
+//         default:
+//             console.log('1 jil 12 sartai !')
+//     }
+
+ // Гурвалжин эсэх
+
+// var a, b, c;
+// a = 3;
+// b = 4;
+// c = 5;
+// if(a + b > c && a + c > b && b + c > a){
+//     console.log('YES')
+// }else{
+//     console.log('NO')
+// }
+
+ // Тооны хүрд
+
+//  var n = 3;
+//  for(var i=1; i<=10; i++){
+//     console.log(n + ' * ' + i + ' = ' + (n*i))
+//  }
+
+ // Тооны зэргийн хүрд
+
+//  var n = 3;
+//  for(var i=1; i<=5; i++){
+//     console.log(n + '^' + i + ' = ' + (n ** i))
+//  }
+
+ // Илэрхийлэл 
+
+//  var a, b, c, d;
+//  b = 2;
+//  c = 3;
+//  d = 4;
+//         console.log(a = b * c - d)
+
